6c4b695d288e6c46c6d86f27b64d50505c0a2b04
leejh50-dotcom <leejh50@gmail.com>
2026-02-21 16:06:41 +0900

    Create sw-sv4.js

---
+++ sw-sv4.js
@@ -0,0 +1,29 @@
+const CACHE = "sv4-cache-v1";
+const ASSETS = [
+  "/parkgolf-score/sv4.html",
+  "/parkgolf-score/manifest-sv4.json",
+  "/parkgolf-score/sv4-180.png",
+  "/parkgolf-score/sv4-192.png",
+  "/parkgolf-score/sv4-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request)));
+  }
+});
